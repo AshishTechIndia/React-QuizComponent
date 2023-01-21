48d69337fe6453f7505a85aaf7fe3b2e59851a2a
Ashish Shaw <ajorden58@gmail.com>
2023-01-21 23:08:51 +0530

    commit 3.1

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -5,7 +5,7 @@ class QuizQuestionButton extends Component {
   render() {
     return (
         
-            <li><button text={this.props.button_text}/></li>
+            <li><button>{this.props.button_text}</button></li>
           
     )
   }
